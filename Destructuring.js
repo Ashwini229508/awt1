3cf52f36c2a29d29b621abd3b3e106262029c2be
Ashwini229508 <gouraashwini55@gmail.com>
2025-01-10 23:28:16 +0530

    commit

--- Destructuring.js
+++ Destructuring.js
@@ -1,14 +1,22 @@
-const user = {
-    id: 1,
-    profile: {
-      name: 'Alice',
-      age: 25,
-    },
-  };
+//class
+// const user = {
+//     id: 1,
+//     profile: {
+//       name: 'Alice',
+//       age: 25,
+//     },
+//   };
   
-  const {
-    profile: { name, age },
-  } = user;
-  console.log(name); 
-  console.log(age);  
+//   const {
+//     profile: { name, age },
+//   } = user;
+//   console.log(name); 
+//   console.log(age);  
   
+  //Array
+const points = [1, [2, 3], 4];
+const [a, [b, c], d] = points;
+console.log(a); 
+console.log(b); 
+console.log(c); 
+console.log(d); 
